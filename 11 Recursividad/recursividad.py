67d70cef65e8d29a573122a389d5d453cc8d414a
Francisco-Ez <franciscoalarcon.ez@gmail.com>
2025-06-09 12:51:28 -0300

    Update recursividad.py

--- 11 Recursividad/recursividad.py
+++ 11 Recursividad/recursividad.py
@@ -197,6 +197,7 @@ else:
 
 
 
+
 #8)Escribí una función recursiva llamada contar_digito(numero, digito) que reciba un
 #número entero positivo (numero) y un dígito (entre 0 y 9), y devuelva cuántas veces
 #aparece ese dígito dentro del número.
